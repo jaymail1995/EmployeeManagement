6913895412ffec796658a2262891097f2072448a
Mitul Kheni <mkheni8343@conestogac.on.ca>
2021-03-02 22:52:37 -0500

    Completed CRUD

--- EmployeeManagment.Data/UserData.cs
+++ EmployeeManagment.Data/UserData.cs
@@ -32,6 +32,48 @@ namespace EmployeeManagment.Data
                 return -1;
             }
         }
+        
+        public int UpdateEmployeeDetails(EmployeeModel employeeObject)
+        {
+            try
+            {
+                _query = $"Update Employee SET FirstName = '{employeeObject.EmployeeFirstName}', City = '{employeeObject.EmployeeCity}', Phone = '{employeeObject.EmployeePhone}', Email = '{employeeObject.EmployeeEmail}';";
+                int result = dataManager.UpdateData(_query);
+                if (result > 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+
+        public int DeleteEmployeeDetails(EmployeeModel employeeObject)
+        {
+            try
+            {
+                _query = $"delete from Employee Where Id = {employeeObject.EmployeeID};";
+                int result = dataManager.DeleteData(_query);
+                if(result > 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
 
         public List<EmployeeModel> GetEmployeeList()
         {
